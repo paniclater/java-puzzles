5cd8dee85e966f05b712954218ff5b9b7feb964c
paniclater <paniclater@gmail.com>
2014-05-27 09:43:26 -0600

    some refactoring of test

--- src/com/paniclater/intstractor/TestInstractor.java
+++ src/com/paniclater/intstractor/TestInstractor.java
@@ -1,10 +1,13 @@
 package com.paniclater.intstractor;
 
 public class TestInstractor {
-	Boolean testInstractor() {
-		return new Intstractor().doIt("dywi23jssi88sjdhj1") == 22;
-	}
+	
 	public static void main(String[] args) {
-		System.out.println(new TestInstractor().testInstractor());
+		Intstractor intstractor = new Intstractor();
+		System.out.println(intstractor.doIt("dywi23jssi88sjdhj1") == 22);
+		System.out.println(intstractor.doIt("Dummy 99 Dummy 99") == 36);
+		System.out.println(intstractor.doIt("1") == 1);
+		System.out.println(intstractor.doIt("") == 0);
+		
 	}
 }
